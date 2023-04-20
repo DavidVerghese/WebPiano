9638fccf4f80e75fea18556895354939efd718e3
David Verghese <dj.verghese@gmail.com>
2023-04-20 12:47:57 -0400

    delete some unused code from App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import './App.css';
-import * as Tone from 'tone';
 import Piano from './components/Piano/Piano';
-const useTonePlayer = (note) => {
-
-  const playSound = () => {
-    const newPlayer = new Tone.Synth().toDestination();
-    newPlayer.triggerAttackRelease(note, "8n");
-  };
-  return { playSound };
-};
-
 
 function App() {
-  const { playSound } = useTonePlayer('C4');
   return (
     <div>
       <Piano/>
-      {/* <button onClick={playSound}>Play Sound</button> */}
     </div>
   );
 }
