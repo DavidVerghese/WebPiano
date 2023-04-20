c2b37b793fc8c9eef210389dbeb8a6f4b76d7445
David Verghese <dj.verghese@gmail.com>
2023-04-20 12:16:21 -0400

    moved scales over to their own file

--- src/components/Piano/Piano.js
+++ src/components/Piano/Piano.js
@@ -3,60 +3,8 @@ import './Piano.css';
 import { useState } from 'react';
 import SelectedScaleDropdown from "../SelectedScaleDropdown/SelectedScaleDropdown";
 import SelectedSoundDropdown from "../SelectedSoundDropdown/SelectedSoundDropdown";
-
+import { chromatic, major, minor, pentatonic, blues } from "../Scales/Scales";
 function Piano() {
-  const chromatic = [
-    { note: "C4", color: "white"},
-    { note: "C#4", color: "black" },
-    { note: "D4", color: "white"},
-    { note: "D#4", color: "black" },
-    { note: "E4", color: "white"},
-    { note: "F4", color: "white"},
-    { note: "F#4", color: "black"},
-    { note: "G4", color: "white" },
-    { note: "G#4", color: "black"},
-    { note: "A4", color: "white"},
-    { note: "A#4", color: "black"},
-    { note: "B4", color: "white" },
-    { note: "C5", color: "white" },
-  ];
-  const major = [
-    { note: "C4", color: "gray" },
-    { note: "D4", color: "white" },
-    { note: "E4", color: "white" },
-    { note: "F4", color: "white" },
-    { note: "G4", color: "white" },
-    { note: "A4", color: "white" },
-    { note: "B4", color: "white" },
-    { note: "C5", color: "gray" },
-  ];
-  const minor = [
-    { note: "C4", color: "gray" },
-    { note: "D4", color: "white" },
-    { note: "Eb4", color: "white" },
-    { note: "F4", color: "white" },
-    { note: "G4", color: "white" },
-    { note: "Ab4", color: "white" },
-    { note: "Bb4", color: "white" },
-    { note: "C5", color: "gray" },
-  ];
-  const pentatonic = [
-    { note: "C4", color: "gray" },
-    { note: "Eb4", color: "white" },
-    { note: "F4", color: "white" },
-    { note: "G4", color: "white" },
-    { note: "Bb4", color: "white" },
-    { note: "C5", color: "gray" },
-  ];
-  const blues = [
-    { note: "C4", color: "gray" },
-    { note: "Eb4", color: "white" },
-    { note: "F4", color: "white" },
-    { note: "F#4", color: "white" },
-    { note: "G4", color: "white" },
-    { note: "Bb4", color: "white" },
-    { note: "C5", color: "gray" },
-  ];
   const [selectedScale, setSelectedScale] = useState(chromatic);
   const [selectedScaleName, setSelectedScaleName] = useState('chromatic');
   const [selectedSound, setSelectedSound] = useState('default');
---
+++ src/components/Scales/Scales.js
@@ -0,0 +1,53 @@
+const chromatic = [
+  { note: "C4", color: "white"},
+  { note: "C#4", color: "black" },
+  { note: "D4", color: "white"},
+  { note: "D#4", color: "black" },
+  { note: "E4", color: "white"},
+  { note: "F4", color: "white"},
+  { note: "F#4", color: "black"},
+  { note: "G4", color: "white" },
+  { note: "G#4", color: "black"},
+  { note: "A4", color: "white"},
+  { note: "A#4", color: "black"},
+  { note: "B4", color: "white" },
+  { note: "C5", color: "white" },
+];
+const major = [
+  { note: "C4", color: "gray" },
+  { note: "D4", color: "white" },
+  { note: "E4", color: "white" },
+  { note: "F4", color: "white" },
+  { note: "G4", color: "white" },
+  { note: "A4", color: "white" },
+  { note: "B4", color: "white" },
+  { note: "C5", color: "gray" },
+];
+const minor = [
+  { note: "C4", color: "gray" },
+  { note: "D4", color: "white" },
+  { note: "Eb4", color: "white" },
+  { note: "F4", color: "white" },
+  { note: "G4", color: "white" },
+  { note: "Ab4", color: "white" },
+  { note: "Bb4", color: "white" },
+  { note: "C5", color: "gray" },
+];
+const pentatonic = [
+  { note: "C4", color: "gray" },
+  { note: "Eb4", color: "white" },
+  { note: "F4", color: "white" },
+  { note: "G4", color: "white" },
+  { note: "Bb4", color: "white" },
+  { note: "C5", color: "gray" },
+];
+const blues = [
+  { note: "C4", color: "gray" },
+  { note: "Eb4", color: "white" },
+  { note: "F4", color: "white" },
+  { note: "F#4", color: "white" },
+  { note: "G4", color: "white" },
+  { note: "Bb4", color: "white" },
+  { note: "C5", color: "gray" },
+];
+export { chromatic, major, minor, pentatonic, blues };
