fae3269143b328f2593555860ab8bab95169da7b
David Verghese <dj.verghese@gmail.com>
2023-05-13 15:41:41 -0400

    condense Warning and Piano into one component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Piano from './components/Piano/Piano';
 import Warning from './components/Warning/Warning';
-
+import Interface
+  from './components/Interface/Interface';
 function App() {
   return (
     <div>
-      <Warning/>
-      <Piano/>
+      <Interface/>
     </div>
   );
 }
---
+++ src/components/Interface/Interface.jsx
@@ -0,0 +1,10 @@
+import Piano from "../Piano/Piano";
+import Warning from "../Warning/Warning";
+
+function Interface() {
+  return (<div className="interface">
+    <Piano />
+    <Warning/>
+  </div>)
+}
+export default Interface;
