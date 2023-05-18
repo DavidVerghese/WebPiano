de36e940cd904639b76408f6b2154686814235eb
David Verghese <dj.verghese@gmail.com>
2023-05-17 23:06:42 -0400

    create NotePlayer

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import Piano from './components/Piano/Piano';
 import Warning from './components/Warning/Warning';
 import Interface
   from './components/Interface/Interface';
+import NotePlayer from './components/NotePlayer/NotePlayer';
+
 function App() {
   return (
     <div>
       <Interface width={400} height={40} />
+      <NotePlayer note={"C4"} keyToPlay={"a"} />
+      <NotePlayer note={"D4"} keyToPlay={"s"} />
+      <NotePlayer note={"E4"} keyToPlay={"d"} />
     </div>
   );
 }
---
+++ src/components/NotePlayer/NotePlayer.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import * as Tone from 'tone';
+
+function NotePlayer({ note, keyToPlay }) {
+  let isPlaying = false;
+  useEffect(() => {
+    const synth = new Tone.Synth().toDestination();
+
+    const handleKeyDown = (event) => {
+      if (event.key === keyToPlay && !isPlaying) {
+        synth.triggerAttack(note);
+        isPlaying = true;
+      }
+    };
+
+    const handleKeyUp = (event) => {
+      if (event.key === keyToPlay) {
+        synth.triggerRelease();
+        isPlaying = false;
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keyup', handleKeyUp);
+    };
+  }, [note, keyToPlay]);
+
+  return <div>Press and hold '{keyToPlay}' to play {note}</div>;
+}
+
+export default NotePlayer;
